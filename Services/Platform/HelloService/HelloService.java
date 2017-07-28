3bcf2aa94f51006a481f11900163126d658ca962
Ramakrishna Maddela <ramakrishna.maddela@hp.com>
2017-07-28 12:46:20 +0530

    Added new Services

---
+++ Services/Platform/HelloService/HelloService.java
@@ -0,0 +1,5 @@
+public class HelloService {
+    public static void main(String args []){
+        System.out.println("Hello Service Executed");
+    }
+}
---
+++ Services/Platform/MathService/MathService.java
@@ -0,0 +1,5 @@
+public class MathService {
+    public static void main(String args []){
+        System.out.println("Math Service Executed");
+    }
+}
